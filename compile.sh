ab07c9d5d1b08813ce049f914f512de15c9c3e3c
Alexis Bedoret <alex@heatherbrooklyn.com>
2013-05-22 09:39:18 -0400

    compiling script

--- compile.sh
+++ compile.sh
@@ -2,6 +2,8 @@
 
 echo "Compiling AlarmClock..."
 
+cp --remove-destination bicolor8x8.pde /home/pi/Adafruit-LED-Backpack-Library/examples/bicolor8x8/
+
 cd /home/pi/raspberry-pi-bicolor-matrix-example/
 rm Print.o Wire.o WString.o main.o Adafruit_LEDBackpack.o Adafruit_GFX.o twi.o
 make
